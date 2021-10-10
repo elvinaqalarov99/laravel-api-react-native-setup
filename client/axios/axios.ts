859ba8e9160ab40d87839de605721ec6058ab93f
elvinaqalarov99 <64695589+elvinaqalarov99@users.noreply.github.com>
2021-10-11 00:38:01 +0400

    Some bugs fixed

--- client/axios/axios.ts
+++ client/axios/axios.ts
@@ -2,13 +2,11 @@ import axios, { AxiosRequestConfig } from "axios";
 import * as SecureStore from "expo-secure-store";
 
 const instance = axios.create({
-  baseURL: "http://192.168.0.106:8000/api/v1",
+  baseURL: "http://{YOUR_IP}:8000/api/v1",
   headers: {
     Accept: "application/json",
   },
 });
-
-// home: 192.168.0.106 | workplace: 192.168.31.44
 
 instance.interceptors.request.use(
   async function (config: AxiosRequestConfig) {
